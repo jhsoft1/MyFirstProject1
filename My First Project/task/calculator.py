bf2a7e9133277418bc204b6f6844208ef4458e71
jensh <jens@petja.de>
2023-05-21 09:28:24 +0200

    Initial commit

---
+++ My First Project/task/calculator.py
@@ -0,0 +1,12 @@
+print('''Earned amount:
+Bubblegum: $202
+Toffee: $118
+Ice cream: $2250
+Milk chocolate: $1680
+Doughnut: $1075
+Pancake: $80
+
+Income: $5405.0''')
+staff = int(input("Staff expenses:"))
+other = int(input("Other expenses:"))
+print("Net income: $", 5405 - staff - other)
